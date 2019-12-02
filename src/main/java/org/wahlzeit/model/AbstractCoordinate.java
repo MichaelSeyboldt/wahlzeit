20977ab055c076c45ef38322c8556dec5439b598
ghostyhands <ghostyhands@rommelwood.de>
2019-12-02 11:02:06 +0100

    added pre-, postconditions and invariants for Coordinates

--- src/main/java/org/wahlzeit/model/AbstractCoordinate.java
+++ src/main/java/org/wahlzeit/model/AbstractCoordinate.java
@@ -1,23 +1,46 @@
 package org.wahlzeit.model;
 
 
-import javax.swing.*;
-
 public abstract class AbstractCoordinate implements ICoordinate {
 
     private void testNotNull(ICoordinate coordinate){
         if(coordinate==null) throw new IllegalArgumentException();
     }
 
+    protected void assertUsable(double x, String msg){
+        if(Double.isNaN(x)){
+            throw new IllegalStateException("is NaN: " + msg);
+        }
+        if (Double.isInfinite(x)){
+            throw new IllegalStateException("is infinite: " + msg);
+        }
+    }
+    protected void assertUsableArg(double x, String msg){
+        if(Double.isNaN(x)){
+            throw new IllegalArgumentException("is NaN: " + msg);
+        }
+        if(Double.isInfinite(x)){
+            throw new IllegalArgumentException("is infinite" + msg);
+        }
+    }
+
+    protected abstract void assertInvariant() ;
+
+
     private double doGetCentralAngle(ICoordinate coordinate){
+
         SphericCoordinate from = this.asSphericCoordinate();
         SphericCoordinate target = coordinate.asSphericCoordinate();
+
         return Math.acos(Math.sin(from.theta) * Math.sin(target.theta) + (Math.cos(from.theta) * Math.cos(target.theta) * Math.cos(target.phi-from.phi)));
+
     }
 
     private double doGetCartesianDistance(ICoordinate coordinate){
+
         CartesianCoordinate from = this.asCartesianCoordinate();
         CartesianCoordinate to = coordinate.asCartesianCoordinate();
+
         double dx = to.x - from.x;
         double dy = to.y - from.y;
         double dz = to.z - from.z;
@@ -25,14 +48,33 @@ public abstract class AbstractCoordinate implements ICoordinate {
     }
     @Override
     public double getCentralAngle(ICoordinate coordinate){
+        //precon
         testNotNull(coordinate);
-        return doGetCentralAngle(coordinate);
+
+        //invariant
+        this.assertInvariant();
+        if(coordinate instanceof AbstractCoordinate) {
+            ((AbstractCoordinate) coordinate).assertInvariant();
+        }
+        double res = doGetCentralAngle(coordinate);
+        assertUsable(res,"getCentralAngle: result out of bounds: NaN");
+        return res;
     }
 
     @Override
     public double getCartesianDistance(ICoordinate coordinate){
+        //precon
         testNotNull(coordinate);
-        return doGetCartesianDistance(coordinate);
+        //invariant
+        assertInvariant();
+        if(coordinate instanceof AbstractCoordinate) {
+            ((AbstractCoordinate) coordinate).assertInvariant();
+        }
+
+        double res = doGetCartesianDistance(coordinate);
+        //postcon
+        assertUsable(res,"getCartesianDistance: result out of bounds: NaN");
+        return res;
     }
 
 
--- src/test/java/org/wahlzeit/model/LocationTest.java
+++ src/test/java/org/wahlzeit/model/LocationTest.java
@@ -1,6 +1,7 @@
 package org.wahlzeit.model;
 
 import com.google.appengine.repackaged.com.google.gson.internal.$Gson$Preconditions;
+import net.bytebuddy.dynamic.scaffold.FieldLocator;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,38 +13,37 @@ public class LocationTest {
     private SphericCoordinate sph0, sph1, sph2;
 
     @Before
-    public void setup(){
-        zero = new CartesianCoordinate(0 ,0,0);
-        tenx = new CartesianCoordinate( 10,0,0);
-        teny = new CartesianCoordinate( 0,10,0);
-        tenz = new CartesianCoordinate( 0 ,0,10);
-        sph0 = new SphericCoordinate(1,0,0);
-        sph1 = new SphericCoordinate(1,Math.PI,0);
-        sph2 = new SphericCoordinate(1,0,0);
+    public void setup() {
+        zero = new CartesianCoordinate(0, 0, 0);
+        tenx = new CartesianCoordinate(10, 0, 0);
+        teny = new CartesianCoordinate(0, 10, 0);
+        tenz = new CartesianCoordinate(0, 0, 10);
+        sph0 = new SphericCoordinate(1, 0, 0);
+        sph1 = new SphericCoordinate(1, Math.PI, 0);
+        sph2 = new SphericCoordinate(1, 0, 0);
 
     }
 
 
     @Test
-    public void testDistancesCartesian(){
-        assertEquals(zero.getCartesianDistance(zero),0., 1e-15);
-        assertEquals(zero.getCartesianDistance(tenx),10.,1e-15);
-        assertEquals(zero.getCartesianDistance(teny),10.,1e-15);
-        assertEquals(zero.getCartesianDistance(tenz),10.,1e-15);
+    public void testDistancesCartesian() {
+        assertEquals(zero.getCartesianDistance(zero), 0., 1e-15);
+        assertEquals(zero.getCartesianDistance(tenx), 10., 1e-15);
+        assertEquals(zero.getCartesianDistance(teny), 10., 1e-15);
+        assertEquals(zero.getCartesianDistance(tenz), 10., 1e-15);
 
     }
 
     @Test
-    public void testAngleSpheric(){
-        assertEquals(sph0.getCentralAngle(sph1),Math.PI,1e-10);
-        assertEquals(sph0.getCentralAngle(sph2),0,1e-10);
-        assertEquals(sph1.getCentralAngle(sph1),0,1e-10);
+    public void testAngleSpheric() {
+        assertEquals(sph0.getCentralAngle(sph1), Math.PI, 1e-10);
+        assertEquals(sph0.getCentralAngle(sph2), 0, 1e-10);
+        assertEquals(sph1.getCentralAngle(sph1), 0, 1e-10);
     }
 
 
-
     @Test
-    public void testequalityCartesian(){
+    public void testequalityCartesian() {
         CartesianCoordinate tx = new CartesianCoordinate(10, 0, 0);
         assertTrue(zero.equals(zero));
         assertFalse(zero.equals(tenx));
@@ -59,7 +59,7 @@ public class LocationTest {
     }
 
     @Test
-    public void testEqualitySpheric(){
+    public void testEqualitySpheric() {
         assertTrue(sph0.equals(sph0));
         assertTrue(sph0.equals(sph2));
         assertFalse(sph0.equals(sph1));
@@ -67,12 +67,29 @@ public class LocationTest {
     }
 
     @Test
-    public void  testEqalityConversion(){
-        assertEquals(sph0,sph0.asCartesianCoordinate().asSphericCoordinate());
+    public void testEqalityConversion() {
+        assertEquals(sph0, sph0.asCartesianCoordinate().asSphericCoordinate());
         assertTrue(sph0.equals(sph0.asCartesianCoordinate()));
         assertEquals(zero, zero.asSphericCoordinate().asCartesianCoordinate());
         assertTrue(zero.equals(zero.asSphericCoordinate()));
 
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstrCart() {
+        CartesianCoordinate nah = new CartesianCoordinate(Double.NaN, 0, 0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstrSpher(){
+        SphericCoordinate nah = new SphericCoordinate(-50, -5, -9);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testToooFaaarAwaaay(){
+        CartesianCoordinate inAGalaxy = new CartesianCoordinate(Double.MAX_VALUE,0,0);
+        CartesianCoordinate faaarFaaarAwaaay = new CartesianCoordinate(0,0,Double.MAX_VALUE);
+        inAGalaxy.getCartesianDistance(faaarFaaarAwaaay);
+
+    }
 }
